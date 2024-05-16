ad157c98a42dcecfa43340bc143912b7f4a09e29
synbyte <rashnikov2.0@gmail.com>
2024-05-16 19:34:41 +0000

    added client page

--- p360/app/clients/page.tsx
+++ p360/app/clients/page.tsx
@@ -2,4 +2,21 @@ import { createClient } from "@/utils/supabase/server";
 
 export default async function Clients() {
     const supabase = createClient()
+    const { data: clients } = await supabase.from("clients").select("*")
+
+    return (
+        <div>
+            <ul>
+                {clients?.map((client) => (
+                    <li key={client.id}><p className="underline">{client.first_name} {client.last_name}</p>
+                    <p>Email: {client.email}</p>
+                    <p>Phone Number: {client.phone}</p>
+                    <p>Birthday: {client.dob}</p>
+                    <p>Address: {client.address}</p>
+                    </li>
+                    
+                ))}
+            </ul>
+        </div>
+    )
 }
