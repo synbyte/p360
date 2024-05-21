f4b18f40fd84257eceb50130ec53aa862d9ba99e
synbyte <rashnikov2.0@gmail.com>
2024-05-21 23:20:40 +0000

    fixed milestones to goals

--- p360/app/clients/[id]/goals/[goal]/milestones/page.tsx
+++ p360/app/clients/[id]/goals/[goal]/milestones/page.tsx
@@ -5,18 +5,13 @@ export default async function Page({ params }:{params:{goal: string}}) {
     const supabase = createClient()
     const { data: milestones } = await supabase.from("milestones").select("*").eq("parent_goal", params.goal)
     
-const { data: goals, error } = await supabase
-.from('goals')
-.select(`
-  id,
-  milestones (
-    id
-  )
-`)
-console.log(goals[4].milestones)
+    
+    return( 
+    <div className='container flex flex-col flex-1 items-center'>
+        <p className='text-3xl font-bold'>Milestones</p>
+        <div className='w-full bg-gradient-to-r from-transparent to-transparent via-foreground/10 p-[1px] my-8'></div>
 
-    return(
-        <ul className="divide-y">
+    <ul className="divide-y">
             {milestones?.map((milestone) => (<li className="py-2" key={milestone.id}>
                 <p>{milestone.description}</p>
                 <p className="text-sm">{milestone.notes}</p>
@@ -25,5 +20,6 @@ console.log(goals[4].milestones)
                 </li>
             ))}
         </ul>
+        </div>
     )
 }
--- p360/app/clients/[id]/goals/page.tsx
+++ p360/app/clients/[id]/goals/page.tsx
@@ -31,22 +31,27 @@ export default async function Page({ params }: { params: { id: string } }) {
             <div className="w-full bg-gradient-to-r from-transparent to-transparent via-foreground/10 p-[1px] my-8"></div>
             <div className="grid grid-cols-2 gap-3 max-w-screen-md">
                 {goals?.map((goal) => (
+                    
                     <Link
                         href={`/clients/${params.id}/goals/${goal.id}/milestones`}
                         key={goal.id}
                     > 
-                        <div key={goal.id} className="p-3 rounded border text-start">
-                            <input
+                        <div key={goal.id} className="flex flex-col p-3 rounded border text-start">
+                            <div className='flex justify-between'>
+                                <div><input
                                 className="mr-2"
                                 checked={goal.is_done}
                                 readOnly
                                 type="checkbox"
                                 name="is_done"
                                 id="is_done"
-                            />
+                            /> 
                             <label htmlFor="is_done">
                                 {goal.is_done ? "Completed" : "In Progress"}
-                            </label>
+                            </label></div>
+                            <p className='inline-block self-end'>{milestones?.filter((stone) => stone.parent_goal === goal.id).length} Milestones </p>
+                            </div>
+                            
                             <p className="text-xl underline">{goal.name}</p>
                             <p>{goal.description}</p>
                         </div>
