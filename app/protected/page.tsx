3babedd515ac06ee2141389add072d01f7cae48d
synbyte <rashnikov2.0@gmail.com>
2024-05-30 19:27:45 +0000

    updated home page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -17,14 +17,20 @@ export default async function ProtectedPage() {
     return redirect("/login");
   }
 
+  const { data:clients } = await supabase.from('clients').select('id')
+  const { data:goals } = await supabase.from('goals').select('id')
+  const { data:milestones } = await supabase.from('milestones').select('id')
   return (
-    <div className="flex flex-col flex-1 gap-20 items-center w-full">
+    <div className="flex flex-col flex-1 gap-20 items-center py-10 w-full">
      
 
       <div className="flex flex-col flex-1 gap-20 px-3 max-w-4xl opacity-0 animate-in">
-        <Header />
+        
         <main className="flex flex-col flex-1 gap-6">
-          <h2 className="mb-4 text-4xl font-bold">Next steps</h2>
+          <h2 className="mb-4 text-3xl font-bold">Logged in as {user.email}</h2>
+          <div className='flex'><h2 className='text-4xl font-bold'> {clients?.filter((client) => client.id).length}</h2><p className='px-2 text-xl'>registered clients</p></div>
+          <div className='flex'><h2 className='text-4xl font-bold'> {goals?.filter((goal) => goal.id).length}</h2><p className='px-2 text-xl'> goals</p></div>
+          <div className='flex'><h2 className='text-4xl font-bold'> {milestones?.filter((stone) => stone.id).length}</h2><p className='px-2 text-xl'>milestones</p></div>
          
         </main>
       </div>
