46c5d1795172aa76ff8991b6327ec25333aaa8fa
synbyte <rashnikov2.0@gmail.com>
2024-05-30 19:15:08 +0000

    updated shadow on sidebar/navbar

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,7 +1,7 @@
 export default function SideNav() {
 
     return (
-        <div className='flex fixed left-0 top-16 z-50 flex-col px-5 py-10 w-1/5 h-full border-r shadow-lg border-r-foreground/10 bg-slate-100'>
+        <div className='flex fixed left-0 top-16 z-50 flex-col px-5 py-10 w-1/5 h-full border-r shadow border-r-foreground/10 bg-slate-100'>
             <label className='text-xs uppercase' htmlFor="">search</label>
             <div className='flex'><input className='py-1 pl-2 mx-1 rounded border transition outline-blue-400 focus:shadow-md' type="text" /><button className='px-1 py-0 rounded border transition active:shadow-lg hover:bg-btn-background'>Search</button></div>
         </div>
