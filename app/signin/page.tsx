3879a2324446c802c423e6adf45275e00e96dc0c
synbyte <rashnikov2.0@gmail.com>
2024-06-20 19:30:01 +0000

    adding new shadnc form

---
+++ app/signin/page.tsx
@@ -0,0 +1,119 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+
+const formSchema = z.object({
+  fname: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  lname: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  phone: z.string().min(10, {
+    message: "Username must be a valid phone number.",
+  }),
+})
+
+export default function ProfileForm() {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fname: "",
+      lname: "",
+      phone: '',
+    },
+  })
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <Card>
+        <CardHeader>
+            <CardTitle>Welcome to Pier360</CardTitle> 
+        </CardHeader>
+        <CardContent className="flex flex-col">
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div className='md:flex'>
+            <div className='px-3 md:w-1/3'>
+        <FormField
+          control={form.control}
+          name="fname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='text-xs font-bold uppercase'>First name</FormLabel>
+              <FormControl>
+                <Input placeholder="Your name" {...field} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        </div>
+        <div className='px-3 md:w-1/3'>
+        <FormField
+          control={form.control}
+          name="lname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='text-xs font-bold uppercase'>First name</FormLabel>
+              <FormControl>
+                <Input placeholder="Your name" {...field} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        </div>
+        <div className='px-3 md:w-1/3'>
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className='text-xs font-bold uppercase'>First name</FormLabel>
+              <FormControl>
+                <Input placeholder="Your name" {...field} />
+              </FormControl>
+              
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        </div>
+        </div>
+        <div className='md:flex'>
+            <div className='px-3 w-full md:w-1/2'>
+            =
+            </div>
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+    </CardContent>
+    </Card>
+  )
+}
